d776eaa65ab45e50b05421ea4ffbf1cf90ec288b
João Victor Vieira Marcondes <166854246+joaobgoode@users.noreply.github.com>
2025-10-28 10:43:41 -0300

    Desafio 08 - Mapas

---
+++ Desafio08/Desafio08/Desafio08App.swift
@@ -0,0 +1,17 @@
+//
+//  Desafio08App.swift
+//  Desafio08
+//
+//  Created by Turma01-8 on 28/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Desafio08App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
